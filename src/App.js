d8dabe2efc8000ac20fe52a6d0e1f1564f2b6415
Rodrigo Iturrieta <riturrieta@brainfood.cl>
2023-07-11 23:14:08 -0400

    ...

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { FindVeknid } from './screens/FindVeknId';
-import { Home } from './screens/Home';
+//import { Home } from './screens/Home';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<FindVeknid />} />
           <Route path="/find-veknid" element={<FindVeknid />} />
         </Routes>
       </Router>
